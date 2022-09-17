5e8abdbb9785a67478cea1bd0c088b84933870e4
rohit45kashyap <rohitk@innovatia.co.in>
2022-09-17 18:28:58 +0530

    Mobile navigation

---
+++ js/navigation.js
@@ -0,0 +1,4 @@
+document.getElementById("menu-icon").addEventListener("click", () => {
+    document.getElementById("menu-icon").classList.toggle("cross");
+    document.getElementById("menu-nav").classList.toggle("open");
+})
