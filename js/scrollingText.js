08f9c6b23944b8fd3bcaa59736363eb7699137f4
rohit45kashyap <rohitk@innovatia.co.in>
2022-09-20 12:45:14 +0530

    Added original logo and fixed scroll-text for first scroll --text Integrated creative agency

--- js/scrollingText.js
+++ js/scrollingText.js
@@ -1,8 +1,8 @@
 const texts = [...document.querySelectorAll('.scrolling-text')];
 
-texts.forEach((text) => {
+texts.forEach((text, i) => {
 
-  if(window.innerWidth > 1200) {
+  if((window.innerWidth > 1200) && (i != 0)) {
     const duration = Math.floor(text.getBoundingClientRect().height / 180);
     text.style.animationDuration = `${duration}s`;
   } else {
