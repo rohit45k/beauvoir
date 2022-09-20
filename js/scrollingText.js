c2e2a9f15e270f70e72dcc667746c9a6546f5ff4
rohit45kashyap <rohitk@innovatia.co.in>
2022-09-20 11:51:49 +0530

    animations vertical scroll fixed

--- js/scrollingText.js
+++ js/scrollingText.js
@@ -1,6 +1,12 @@
 const texts = [...document.querySelectorAll('.scrolling-text')];
 
 texts.forEach((text) => {
-  const duration = Math.floor(text.getBoundingClientRect().width / 180);
-  text.style.animationDuration = `${duration}s`;
+
+  if(window.innerWidth > 1200) {
+    const duration = Math.floor(text.getBoundingClientRect().height / 180);
+    text.style.animationDuration = `${duration}s`;
+  } else {
+    const duration = Math.floor(text.getBoundingClientRect().width / 180);
+    text.style.animationDuration = `${duration}s`;
+  }
 });
